a52f305cb12c8df546199f807bfd5d9b9f46955c
VinayakDani <160472075+VinayakDani@users.noreply.github.com>
2024-04-26 21:08:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,12 +16,13 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx =st.connection("snowflake")
 session=cnx.session()
 # session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredient_list = st.multiselect(
     'Choose up to 5 ingredients:', my_dataframe,
-    max_selections=5
-)
+    max_selections=5)
 
 if ingredient_list:
     ingredients_string = ''
@@ -30,18 +31,18 @@ if ingredient_list:
             ingredients_string +=fruit_chosen + ' '
             st.subheader(fruit_chosen+'Nutrition Information'
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"+fruit_chosen)
-            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=TRUE )
+            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True )
     
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
-    time_to_insert = st.button('Submit Order')
+            time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+            session.sql(my_insert_stmt).collect()
         
-        st.success('Your Smoothie is ordered!', icon="✅")
+            st.success('Your Smoothie is ordered!', icon="✅")
 
 
 
