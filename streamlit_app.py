367549c4bd62ad08078a25cfaa54b8127d63b8af
VinayakDani <160472075+VinayakDani@users.noreply.github.com>
2024-04-29 14:12:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,61 +1,17 @@
-# Import python packages
 import streamlit as st
-import requests
-# from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
 
+# Title widget
+st.title('This is a title')
 
+# Plain text widget
+st.text("This is some text")
 
-# # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write("""Choose the fruits you want in your custom smoothie!"""    )
+# Code widget example
+code = '''def hello():
+            print("Hello, Streamlit!")'''
+st.code(code, language='python')
 
-# name_on_order = st.text_input('Name on Smoothie :')
-# st.write('The name on your Smoothie will be:', name_on_order)
-
-# cnx =st.connection("snowflake")
-# session=cnx.session()
-# # session = get_active_session()
-# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-# # st.dataframe(data=my_dataframe, use_container_width=True)
-# # st.stop()
-
-# pd_df = my_dataframe.to_pandas()
-# # st.dataframe(pd_df)
-# # st.stop()
-
-# ingredient_list = st.multiselect(
-#     'Choose up to 5 ingredients:', my_dataframe,
-#     max_selections=5)
-
-# if ingredient_list:
-#     ingredients_string = ''
- 
-#     for fruit_chosen in ingredient_list:
-#             ingredients_string +=fruit_chosen + ' '
-
-#             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-#             st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-            
-#             st.subheader(fruit_chosen+'Nutrition Information')
-#             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"+fruit_chosen)
-#             fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True )
-    
-
-#             my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-#             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
-
-#             time_to_insert = st.button('Submit Order')
-
-#     if time_to_insert:
-#             session.sql(my_insert_stmt).collect()
-        
-#             st.success('Your Smoothie is ordered!', icon="✅")
-
-
-
-
-
-
-
-        
+# Latex widget example
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} = \sum_{k=0}^{n-1} ar^k = a \left(\frac{1-r^{n}}{1-r}\right)
+''')
